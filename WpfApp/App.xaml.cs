64e6524bb5f3dd77c75541b4c279307621f66a5d
Dario Benevento <dario.benevento@georgfischer.com>
2023-05-23 12:42:42 +0200

    Final demo before test

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -1,15 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MudBlazor.Services;
 
 namespace WpfApp;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
+//public partial class App : Application
+//{
+//}
+
 public partial class App : Application
 {
+    public static IHost? AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Host
+            .CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<Modern>();
+                
+
+                Resources.Add("services", services.BuildServiceProvider());
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        var startupForm = AppHost.Services.GetRequiredService<Modern>();
+        startupForm.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+
+        base.OnExit(e);
+    }
 }
